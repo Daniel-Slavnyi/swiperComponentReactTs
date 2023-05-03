34da695e6426cb7a4294eb996c9fe494acef4d84
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-05-03 16:11:51 +0300

    finish first part of working with swiper

--- src/components/SwiperOne/PreviewPhoto/PreviewPhoto.tsx
+++ src/components/SwiperOne/PreviewPhoto/PreviewPhoto.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { CommonClassProps, Photo } from "../types";
 
 import cn from "classnames";
@@ -14,19 +14,34 @@ export const PreviewPhoto: React.FC<PreviewPhotoProps> = ({
   indexOfActivePhoto,
   className,
 }) => {
+  const previewContainer = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (!previewContainer.current) return;
+    previewContainer.current.style.transform = `translateX(-${
+      164 * indexOfActivePhoto
+    }px)`;
+  }, [indexOfActivePhoto]);
+
   return (
     <div className={cn(className, css.previewPhoto)}>
-      <ul className={css.previewPhotoList}>
-        {photos.map((item) => (
-          <li key={item.id} className={css.previewPhotoItem}>
-            <img
-              src={item.preview}
-              alt={item.description}
-              className={css.previewPhotoImg}
-            />
-          </li>
-        ))}
-      </ul>
+      {useMemo(
+        () => (
+          <ul className={css.previewPhotoList} ref={previewContainer}>
+            {photos.map((item) => (
+              <li key={item.id} className={css.previewPhotoItem}>
+                <img
+                  src={item.preview}
+                  alt={item.description}
+                  className={css.previewPhotoImg}
+                />
+              </li>
+            ))}
+          </ul>
+        ),
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+      )}
       <div className={css.previewPhotoWrap}>
         {indexOfActivePhoto + 1} / {photos.length}
       </div>
